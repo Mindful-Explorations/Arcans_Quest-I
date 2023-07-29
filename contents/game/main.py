bc7990f6c6c7481a3a157bd614377ae96c3676ea
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-29 15:05:32 +1000

    Update main.py

--- contents/game/main.py
+++ contents/game/main.py
@@ -1,1 +1,12 @@
-print
+
+
+class Arcan (): 
+
+
+class Narrator():
+
+class Player():
+  def __init__ self():
+  self.name = input("What Is Your first Name \n>> ")
+  self.gender = input("What is your gender \n(What you identify as. e.g Male & Female)\n>> ")
+  
